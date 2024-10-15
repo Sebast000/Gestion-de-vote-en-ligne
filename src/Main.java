86667d1cabc07a85bc28082cbe78bb5deb28873d
Sebast000 <163167593+Sebast000@users.noreply.github.com>
2024-10-15 15:51:28 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        GestionnaireDeVotes gestionnaire = new GestionnaireDeVotes();
+        gestionnaire.initialiserElection(); // Méthode pour initialiser l'élection
+        gestionnaire.demarrerVote(); // Méthode pour démarrer le processus de vote
+    }
+}
 
